4b56a73eac332a734fa77f28709bda04a3d6fea8
Nelreina <nelreina@gmail.com>
2018-02-21 03:43:08 -0400

    Added log4js to project / log to console and file

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const Events = require('events');
 const timer = require('./timer');
+const Log4js = require('log4js');
+Log4js.configure({
+  appenders: {
+    console: { type: 'console' },
+    file: { type: 'file', filename: 'timer.log' }
+  },
+  categories: { default: { appenders: ['console', 'file'], level: 'debug' } }
+});
 
 (async () => {
-  console.info('Program started...');
-
+  const logger = Log4js.getLogger();
+  logger.debug('Program started...');
   const logEvent = new Events();
-  logEvent.on('step', data => console.info(`step ${data}`));
+  logEvent.on('step', data => logger.info(`step ${data}`));
 
-  const done = await timer(4, 3, logEvent);
-  console.info(done);
-  console.info('Program ended!');
+  const done = await timer(10, 0.5, logEvent);
+  logger.info(done);
+  logger.debug('Program ended!');
 })();
