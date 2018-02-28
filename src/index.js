5cbe1f6e5be1f27e940b69d2080e6ad7a81f3809
Nelreina <nelreina@gmail.com>
2018-02-28 02:35:12 -0400

    feature/minimist: added minimist to project

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 const Timer = require('./timer');
 const Log4js = require('log4js');
+const minimist = require('minimist');
+
+const args = minimist(process.argv.slice(2));
+const { iteration, seconds } = args;
 Log4js.configure('./log4js.json');
 const timer = new Timer();
 (async () => {
-  const logger = Log4js.getLogger();
+  const logger = Log4js.getLogger('L');
   logger.debug('Program started...');
+  logger.debug(args);
   timer.on('step', data => logger.info(`step ${data}`));
-
-  const done = await timer.run(100, 0.01);
-  logger.info(done);
+  if (iteration && seconds) {
+    const done = await timer.run(iteration, seconds);
+    logger.info(done);
+  } else {
+    logger.error('Program params are required!');
+    logger.error('Usage e.g "node src/index --iteration=1 --seconds=1"');
+  }
   logger.debug('Program ended!');
 })();
